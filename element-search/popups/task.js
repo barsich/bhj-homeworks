1743e491e27946211a4b2c3f0b95f763af5f77fc
barsich <barsicchan@gmail.com>
2021-03-01 16:45:00 +0500

    for => forEach

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,6 +1,6 @@
 const modalMain = document.getElementById('modal_main');
 const modalSuccess = document.getElementById('modal_success');
-const modalClose = document.getElementsByClassName('modal__close_times');
+const modalClose = Array.from(document.getElementsByClassName('modal__close_times'));
 const btn = document.getElementsByClassName('btn');
 
 modalMain.className = 'modal modal_active';
@@ -10,9 +10,9 @@ btn[0].onclick = function () {
   modalSuccess.className = 'modal modal_active';
 };
 
-for (let i = 0; i < modalClose.length; i++) {
-  modalClose[i].onclick = function () {
+modalClose.forEach((item) => {
+  item.onclick = function () {
     modalMain.className = 'modal';
     modalSuccess.className = 'modal';
   };
-}
+});
